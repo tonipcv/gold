d728bc4a72c48ef6112da29e4c3e9286c2a5546c
tonipcv <xppsalvador@gmail.com>
2025-09-10 17:22:25 -0300

    a

---
+++ src/app/semestral-canal/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function Page() {
+  redirect("https://chat.whatsapp.com/GRJO1LqxQQRG6vdaEEX0nY?mode=ems_copy_c");
+}
---
+++ src/app/trimestral-canal/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function Page() {
+  redirect("https://chat.whatsapp.com/IFa49sBV4FuAI14UvTuRX6?mode=ems_copy_c");
+}

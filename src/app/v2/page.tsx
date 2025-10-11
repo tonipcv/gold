67eeac7d7a87ac3ed161b00368860b3f7f9372d7
tonipcv <xppsalvador@gmail.com>
2025-10-11 15:14:10 -0300

    va

--- src/app/v2/page.tsx
+++ src/app/v2/page.tsx
@@ -4,6 +4,18 @@ import Link from 'next/link'
 import Script from 'next/script'
 import { OptimizedImage } from '../components/OptimizedImage'
 
+// Local JSX declaration to ensure TS recognizes the custom web component during build
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'vturb-smartplayer': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        id?: string
+        style?: React.CSSProperties
+      }
+    }
+  }
+}
+
 // Ensure this runs on the server and revalidates when rebuilt
 export const dynamic = 'force-static'
 
